16cd8e52d211585c6b7961ed8d34c27787e8441a
Noothan am <100135382+Noothan-am@users.noreply.github.com>
2024-12-04 21:55:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const {
 } = require("@google/generative-ai");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 2000;
 app.use(express.json());
 
 const corsOptions = {
